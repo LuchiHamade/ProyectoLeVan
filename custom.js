a36dd711624d320bcc90e3927977bf8e1476c282
Lautaro Rocha <lautarovrocha@gmail.com>
2023-10-03 20:55:39 -0300

    chore. example nav

---
+++ custom.js
@@ -0,0 +1,19 @@
+const navButton = document.querySelector('button')
+const navList = document.querySelector('#navUl')
+
+navButton.onclick = () => {
+    const styles = getComputedStyle(navList)
+    const { left } = styles;
+    if(left === '-200px'){
+        navList.style.left = 0
+    }else{
+        navList.style.left = '-200px'
+    }
+}
+
+window.onresize( () => {
+    if(clientWidth > 760){
+        navList.style.left = '60vw'
+
+    }
+})
