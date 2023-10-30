a365d4fecf75a23b245dd7a69bad0fb745acdc4d
LuchiHamade <luciana.b.hamade@gmail.com>
2023-10-30 20:04:34 -0300

    modifiqué letra de la fuente , agregué cambio de imágenes en js

--- custom.js
+++ custom.js
@@ -16,4 +16,39 @@ window.onresize =  () => {
         navList.stoyle.left = '60vw'
 
     }
+
+    const PICTURES = {
+        inicio: 'https://pixabay.com/es/photos/bolas-de-sushi-comida-japonesa-5878892',
+        contacto: 'https://pixabay.com/es/photos/sushi-men%C3%BA-restaurante-comida-8113165/',
+        menu:'https://pixabay.com/es/photos/sushi-vegetariano-verduras-arroz-2112350/',
+    }
+
+    const figure = document.querySelector('figure')
+
+    const doSomethingAccordingToURL = ()=>{
+        const {pathname} = window.location;
+
+        switch(pathname){
+            case '/':
+                figure.style.backgroundImage = `url(${PICTURES.start})`
+                break;
+            case '/CONTACTO':
+                figure.style.backgroundImage = `url(${PICTURES.contact})`
+                break;
+            case '/MENU':
+                figure.style.backgroundImage = `url(${PICTURES.menu})`
+                break;
+        }
+    }
+    
+    const spans = document.querySelectorAll('span')
+    spans.forEach( (node)=>{
+        node.onclick = (e) => {
+            const content = e.target.textContent
+            window.history.pushState({}, content, content == 'INICIO' ? ' /' : content)
+            doSomethingAccordingToURL()
+        }
+    })
+
+    window.onload = ()=> doSomethingAccordingToURL()
 }
