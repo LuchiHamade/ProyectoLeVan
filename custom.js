3def7ec8b36bcd8d2bd259a9712d33c3de57f76c
Manunyville <125371151+Manunyville@users.noreply.github.com>
2023-10-31 20:11:20 -0300

    Solo funciona la img de inicio

--- custom.js
+++ custom.js
@@ -4,51 +4,51 @@ const navList = document.getElementById('navUl')
 navButton.onclick = () => {
     const styles = getComputedStyle(navList)
     const { left } = styles;
-    if(left === '-200px'){
+    if (left === '-200px') {
         navList.style.left = 0
-    }else{
+    } else {
         navList.style.left = '-200px'
     }
 }
 
-window.onresize =  () => {
-    if(window.innerWidth > 760){
+window.onresize = () => {
+    if (window.innerWidth > 760) {
         navList.stoyle.left = '60vw'
-        
+
     }
 }
 
-    const PICTURES = {
-        inicio: '././assets/images/sushi.inicio.jpg',
-        contacto: '././assets/images/sushi.contacto.jpg',
-        menu:'././assets/images/sushi.menu.jpg',
+const PICTURES = {
+    inicio: '././assets/images/sushi.inicio.jpg',
+    menu: '././assets/images/sushi.menu.jpg',
+    contacto: '././assets/images/sushi.contacto.jpg',
+}
+
+const body = document.querySelector('body')
+
+const doSomethingAccordingToURL = () => {
+    const { pathname } = window.location;
+
+    switch (pathname) {
+        case '/':
+            body.style.backgroundImage = `url(${PICTURES.inicio})`
+            break;
+        case '/MENU':
+            body.style.backgroundImage = `url(${PICTURES.menu})`
+            break;
+        case '/CONTACTO':
+            body.style.backgroundImage = `url(${PICTURES.contacto})`
+            break;
     }
+}
 
-    const body = document.querySelector('body')
-
-    const doSomethingAccordingToURL = ()=>{
-        const {pathname} = window.location;
-
-        switch(pathname){
-            case '/':
-                body.styles.backgroundImage = `url(${PICTURES.start})`
-                break;
-            case '/CONTACTO':
-                body.styles.backgroundImage = `url(${PICTURES.contact})`
-                break;
-            case '/MENU':
-                body.styles.backgroundImage = `url(${PICTURES.menu})`
-                break;
-        }
+const listItems = document.querySelectorAll('li')
+listItems.forEach((node) => {
+    node.onclick = (e) => {
+        const content = e.target.textContent
+        window.history.pushState({}, content, content == 'INICIO' ? ' /' : content)
+        doSomethingAccordingToURL()
     }
-    
-    const listItems = document.querySelectorAll('li')
-    listItems.forEach( (node)=>{
-        node.onclick = (e) => {
-            const content = e.target.textContent
-            window.history.pushState({}, content, content == 'INICIO' ? ' /' : content)
-            doSomethingAccordingToURL()
-        }
-    })
+})
 
-    window.onload = ()=> doSomethingAccordingToURL()
+window.onload = () => doSomethingAccordingToURL()
