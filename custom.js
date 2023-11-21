bb5b69c8cdbaa1066af627ebc7bd5e9f85b05dda
lautroRocha <lautarovrocha@gmail.com>
2023-11-21 20:31:22 -0300

    chore: map menu

--- custom.js
+++ custom.js
@@ -26,9 +26,10 @@ const PICTURES = {
     'Contacto': '/assets/images/sushi.contacto.jpg',
 }
 
-const body = document.querySelector('.background')
+const body = document.querySelector('main')
 const presentacion = document.getElementById('presentacion')
 const svg = document.querySelector('#fondoSvg')
+const menu = document.querySelector('.menu')
 
 const doSomethingAccordingToURL = () => {
     const { pathname } = window.location;
@@ -39,18 +40,22 @@ const doSomethingAccordingToURL = () => {
             presentacion.innerHTML = '<p>Bienvenidos a levan sushi, en nuestra web vas a encontrar todos nuestros combos y muchas mas cosas</p>'
             svg.classList.remove('rotatedMinus180')
             svg.classList.remove('rotated90')
+            emptyMenu()
             break;
         case '/Menu':
             body.style.backgroundImage = `url(${PICTURES.Menu})`
             presentacion.innerHTML = '<p>En esta seccion encontras nuestro menu completo</p>'
             svg.classList.remove('rotatedMinus180')
             svg.classList.add('rotated90')
+            fillMenu()
             break;
         case '/Contacto':
             body.style.backgroundImage = `url(${PICTURES.Contacto})`
             presentacion.innerHTML= '<p>Aqui podras ver nuestras formas de contacto</p>'
             svg.classList.remove('rotated90')
             svg.classList.add('rotatedMinus180')
+            emptyMenu()
+
             break;
     }
 }
@@ -65,3 +70,73 @@ listItems.forEach((node) => {
 })
 
 window.onload = () => doSomethingAccordingToURL()
+
+
+const MENU_ITEMS = [
+    {
+        name: 'Sushi',
+        photo: 'https://images.zeald.com/site/redwasabi/images/sushi-354628_1920.jpg'
+    },
+    {
+        name: 'Sushi',
+        photo: 'https://images.zeald.com/site/redwasabi/images/sushi-354628_1920.jpg'
+    },
+    {
+        name: 'Sushi',
+        photo: 'https://images.zeald.com/site/redwasabi/images/sushi-354628_1920.jpg'
+    },
+    {
+        name: 'Sushi',
+        photo: 'https://images.zeald.com/site/redwasabi/images/sushi-354628_1920.jpg'
+    },
+    {
+        name: 'Sushi',
+        photo: 'https://images.zeald.com/site/redwasabi/images/sushi-354628_1920.jpg'
+    },
+    {
+        name: 'Sushi',
+        photo: 'https://images.zeald.com/site/redwasabi/images/sushi-354628_1920.jpg'
+    },
+    {
+        name: 'Sushi',
+        photo: 'https://images.zeald.com/site/redwasabi/images/sushi-354628_1920.jpg'
+    },
+    {
+        name: 'Sushi',
+        photo: 'https://images.zeald.com/site/redwasabi/images/sushi-354628_1920.jpg'
+    },
+    {
+        name: 'Sushi',
+        photo: 'https://images.zeald.com/site/redwasabi/images/sushi-354628_1920.jpg'
+    },
+    {
+        name: 'Sushi',
+        photo: 'https://images.zeald.com/site/redwasabi/images/sushi-354628_1920.jpg'
+    },
+    {
+        name: 'Sushi',
+        photo: 'https://images.zeald.com/site/redwasabi/images/sushi-354628_1920.jpg'
+    },
+    {
+        name: 'Sushi',
+        photo: 'https://images.zeald.com/site/redwasabi/images/sushi-354628_1920.jpg'
+    }
+   
+]
+
+const createMenuCard = (obj)=>{
+    const {name, photo} = obj;
+    const card = document.createElement('div')
+    const title = document.createElement('h2')
+    const picture = document.createElement('img')
+    card.setAttribute('class', 'card')
+    title.textContent = name;
+    picture.setAttribute('src', photo)
+    card.appendChild(title)
+    card.appendChild(picture)
+    menu.appendChild(card)
+}
+
+const fillMenu = () => MENU_ITEMS.forEach((item)=> createMenuCard(item))
+
+const emptyMenu = () => menu.innerHTML = ''
